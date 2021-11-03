44eb488cbd5213d9eff4d457c2c7a1e4b1789d8c
Sharhad Bashar <sbashar@montrium.com>
2021-11-03 05:32:09 -0400

    added size

--- train_cls.py
+++ train_cls.py
@@ -22,6 +22,8 @@ import os
 from PIL import Image
 from tensorboardX import SummaryWriter
 
+import pickle
+
 
 
 def compute_acc(pred_labels, gt_labels):
@@ -31,17 +33,17 @@ def compute_acc(pred_labels, gt_labels):
         if pred_label in gt_labels:
             pred_correct_count += 1
     union = len(gt_labels) + len(pred_labels) - pred_correct_count
-    acc = round(pred_correct_count/union, 4)
+    acc = round(pred_correct_count / union, 4)
     return acc
 
 
 def cls200_sum(y_200):
     cls20_prob_sum_list = []
     for rou in range(20):
-        subclass_prob_sum = sum(y_200[rou*k_cluster:rou*k_cluster+k_cluster])
-        cls20_prob_sum_list.append(subclass_prob_sum/10)
+        subclass_prob_sum = sum(y_200[rou * k_cluster:rou * k_cluster + k_cluster])
+        cls20_prob_sum_list.append(subclass_prob_sum / 10)
 
-    cls200_pred_max = np.where(np.array(cls20_prob_sum_list)>0.05)[0]
+    cls200_pred_max = np.where(np.array(cls20_prob_sum_list) > 0.05)[0]
     return cls200_pred_max
 
 
@@ -49,8 +51,34 @@ def get_img_path(img_name, dataset_path):
     tmp = os.path.join(dataset_path, img_name + '.jpg')
     return tmp
 
+def get_size_path(img_name, size_dataset_path):
+    tmp = os.path.join(dataset_path, img_name + '.pkl')
+    return tmp
 
+def getAB(size):
+    if size <= 0.1: return [0, 0.1]
+    elif size > 0.1 and size <= 0.2: return [0.1, 0.2]
+    elif size > 0.2 and size <= 0.3: return [0.2, 0.3]
+    elif size > 0.3 and size <= 0.4: return [0.3, 0.4]
+    elif size > 0.4 and size <= 0.5: return [0.4, 0.5]
+    elif size > 0.5 and size <= 0.6: return [0.5, 0.6]
+    elif size > 0.6 and size <= 0.7: return [0.6, 0.7]
+    elif size > 0.7 and size <= 0.8: return [0.7, 0.8]
+    elif size > 0.8 and size <= 0.9: return [0.8, 0.9]
+    elif size > 0.9 and size <= 1  : return [0.9, 1]
 
+def getPenalty(label_size_n, size_n, C = 21):
+    penalty_n = torch.zeros_like(label_size_n)
+    for i in range(C):
+        a, b = getAB(size_n[i])
+        # a, b = [0.15, 1]
+        if label_size_n[i] < a:
+            penalty_n[i] = a
+        elif pred_cls_size_n[i] > b:
+            penalty_n[i] = b
+        else:
+            penalty_n[i] = label_size_n[i]
+    return penalty_n
 
 
 class Sub_Class_Dataset(Dataset):
@@ -83,10 +111,11 @@ class Sub_Class_Dataset(Dataset):
         label_20 = self.label_20[index]
 
         self.dataset_path = os.path.join(self.voc12_root, 'JPEGImages')
+        self.size_dataset_path = os.path.join(self.voc12_root, 'SegmentationClassAugSize')
 
         filename = self.filename[index]
 
-        img = Image.open(get_img_path(filename, self.dataset_path)).convert("RGB")
+        img = Image.open(get_img_path(filename, self.dataset_path)).convert('RGB') #filename = 2007_000032
         img = imutils.ResizeLong(img, 256, 512)
         img = imutils.Flip(img)
         img = imutils.ColorJitter(img)
@@ -96,7 +125,12 @@ class Sub_Class_Dataset(Dataset):
         img = img.transpose(2,0,1)
         img = torch.from_numpy(img)
 
-        return img, label_20, label_200, filename
+        with open(get_size_path(filename, self.size_dataset_path), 'rb') as f:
+            size = pickle.load(f)
+        size = np.array(list(size.values()), dtype = np.float64)
+        size = torch.from_numpy(size)
+
+        return img, size, label_20, label_200, filename
 
     def __len__(self):
         return len(self.filename)
@@ -107,21 +141,21 @@ class Sub_Class_Dataset(Dataset):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=16, type=int)
-    parser.add_argument("--max_epoches", default=61, type=int)
-    parser.add_argument("--network", default="network.resnet38_cls", type=str)
-    parser.add_argument("--lr", default=0.01, type=float)
-    parser.add_argument("--num_workers", default=8, type=int)
-    parser.add_argument("--wt_dec", default=5e-4, type=float)
-    parser.add_argument("--weights", required=True, type=str, help="the path to the pretrained weight ")
-    parser.add_argument("--train_list", default="voc12/train_aug.txt", type=str)
-    parser.add_argument("--session_name", default="resnet_cls", type=str)
-    parser.add_argument("--crop_size", default=224, type=int)
-    parser.add_argument("--voc12_root", default="/home/julia/datasets/VOC2012", type=str, help="the path to the dataset folder")
-    parser.add_argument("--subcls_loss_weight", default="5", type=float, help="the weight multiply to the sub-category loss")
-    parser.add_argument("--round_nb", default="0", type=int, help="the round number of the training classifier, e.g., 1st round: round_nb=1, and so on")
-    parser.add_argument("--k_cluster", default="10", type=int, help="the number of the sub-category")
-    parser.add_argument("--save_folder", required=True, default="./save", type=str, help="the path to save the model")
+    parser.add_argument('--batch_size', default=16, type=int)
+    parser.add_argument('--max_epoches', default=61, type=int)
+    parser.add_argument('--network', default='network.resnet38_cls', type=str)
+    parser.add_argument('--lr', default=0.01, type=float)
+    parser.add_argument('--num_workers', default=8, type=int)
+    parser.add_argument('--wt_dec', default=5e-4, type=float)
+    parser.add_argument('--weights', required=True, type=str, help='the path to the pretrained weight ')
+    parser.add_argument('--train_list', default='voc12/train_aug.txt', type=str)
+    parser.add_argument('--session_name', default='resnet_cls', type=str)
+    parser.add_argument('--crop_size', default=224, type=int)
+    parser.add_argument('--voc12_root', default='/home/julia/datasets/VOC2012', type=str, help='the path to the dataset folder')
+    parser.add_argument('--subcls_loss_weight', default='5', type=float, help='the weight multiply to the sub-category loss')
+    parser.add_argument('--round_nb', default='0', type=int, help='the round number of the training classifier, e.g., 1st round: round_nb=1, and so on')
+    parser.add_argument('--k_cluster', default='10', type=int, help='the number of the sub-category')
+    parser.add_argument('--save_folder', required=True, default='./save', type=str, help='the path to save the model')
 
     args = parser.parse_args()
 
@@ -149,7 +183,6 @@ if __name__ == '__main__':
                                     drop_last=True)
 
 
-
     max_step = (len(train_dataset) // args.batch_size) * args.max_epoches
 
 
@@ -163,11 +196,11 @@ if __name__ == '__main__':
 
 
     if args.weights[-7:] == '.params':
-        assert args.network == "network.resnet38_cls"
+        assert args.network == 'network.resnet38_cls'
         import network.resnet38d
         weights_dict = network.resnet38d.convert_mxnet_to_torch(args.weights)
     elif args.weights[-11:] == '.caffemodel':
-        assert args.network == "network.vgg16_cls"
+        assert args.network == 'network.vgg16_cls'
         import network.vgg16d
         weights_dict = network.vgg16d.convert_caffe_to_torch(args.weights)
     else:
@@ -179,10 +212,9 @@ if __name__ == '__main__':
 
     avg_meter = pyutils.AverageMeter('loss')
 
-    timer = pyutils.Timer("Session started: ")
+    timer = pyutils.Timer('Session started: ')
 
-
-    parent_labels = np.load('./voc12/cls_labels.npy', allow_pickle = True).tolist()
+    parent_labels = np.load('./voc1/cls_labels.npy', allow_pickle = True).tolist()
     k_cluster = 10
 
 
@@ -199,13 +231,12 @@ if __name__ == '__main__':
         cls20_ep_EM = 0
         cls20_ep_acc = 0
 
-        for iter, (data, label_20, label_200, filename) in tqdm(enumerate(train_data_loader)):
+        for iter, (data, size, label_20, label_200, filename) in tqdm(enumerate(train_data_loader)):
 
             img = data
             label_20 = label_20.cuda(non_blocking=True)
             label_200 = label_200.cuda(non_blocking=True)
             img_name = filename
-
 
             x_20, _, y_20, x_200, y_200 = model(img, args.round_nb)
 
@@ -228,7 +259,7 @@ if __name__ == '__main__':
             tmp = y_200.cpu().data.numpy()
             tmp_label = label_200.cpu().data.numpy()
             for num, one in enumerate(tmp):
-                pass_cls = np.where(one>0.5)[0]
+                pass_cls = np.where(one > 0.5)[0]
                 true_cls_200 = np.where(tmp_label[num] == 1)[0]
 
                 parent_cls = np.where(parent_labels[img_name[num]] == 1)[0]
@@ -243,7 +274,7 @@ if __name__ == '__main__':
                 ep_acc += acc
 
                 # cls200: acc for 200-->20 (top 1)
-                pass_map_cls = np.unique([int(m/k_cluster) for m in np.where(one > 0.5)[0]])
+                pass_map_cls = np.unique([int(m / k_cluster) for m in np.where(one > 0.5)[0]])
                 if np.array_equal(pass_map_cls, parent_cls) == True:
                     ep_p_EM += 1
 
@@ -252,22 +283,58 @@ if __name__ == '__main__':
 
 
 
-            avg_ep_EM = round(ep_EM/ep_count, 4)
-            avg_ep_acc = round(ep_acc/ep_count, 4)
+            avg_ep_EM = round(ep_EM / ep_count, 4)
+            avg_ep_acc = round(ep_acc / ep_count, 4)
 
-            avg_ep_p_EM = round(ep_p_EM/ep_count, 4)
-            avg_ep_p_acc = round(ep_p_acc/ep_count, 4)
+            avg_ep_p_EM = round(ep_p_EM / ep_count, 4)
+            avg_ep_p_acc = round(ep_p_acc / ep_count, 4)
 
-            avg_cls20_ep_EM = round(cls20_ep_EM/ep_count, 4)
-            avg_cls20_ep_acc = round(cls20_ep_acc/ep_count, 4)
+            avg_cls20_ep_EM = round(cls20_ep_EM / ep_count, 4)
+            avg_cls20_ep_acc = round(cls20_ep_acc / ep_count, 4)
 
             cls_20_loss = F.multilabel_soft_margin_loss(x_20, label_20)
             cls_200_loss = F.multilabel_soft_margin_loss(x_200, label_200)
 
-            loss = cls_20_loss + (args.subcls_loss_weight*cls_200_loss)
 
-            if iter%100 ==0:
-                print('k{} R{}| Ep:{} L:{} -20_LOSS:{} -200_LOSS:{} | -cls20:{} | -cls200_Top1:{} -Sum:{}'.format(args.k_cluster, args.round_nb, ep, round(loss.item(), 3), round(cls_20_loss.item(), 3), round(cls_200_loss.item(), 3), avg_cls20_ep_acc, avg_ep_p_acc, avg_ep_acc))
+            # # Sharhad
+            # # loss_1:
+            weight = [1e-3, 1e-2, 1e-1, 1, 10, 20, 50, 100, 200, 500]
+            label_20_softmax = F.softmax(label_20)
+            label_200_softmax = F.softmax(label_200)
+            label_20_size = label_20_softmax.mean(3).mean(2)
+            label_200_size = label_200_softmax.mean(3).mean(2)
+
+            N_c, C = label_20.shape
+            sum_N_c = 0
+            sum_N_c_32x = 0
+
+            for n in range(N_c):
+                sum_C = torch.sum((label_20_size[n] - size[n].cuda()) ** 2)
+                sum_C_32x = torch.sum((label_200_size[n] - size[n].cuda()) ** 2)
+
+                sum_N_c += sum_C / (C - 1)
+                sum_N_c_32x += sum_C_32x / (C - 1)
+
+            loss_size = weight[5] * (sum_N_c + sum_N_c_32x) / N_c
+
+            # #loss_2:
+            # for n in range(N_c):
+            #     pentaly_n = getPenalty(label_20_size[n], size[n])
+            #     sum_C = torch.sum((label_20_size[n] - pentaly_n.cuda()) ** 2)
+
+            #     pentaly32x_n = getPenalty(label_200_size[n], size[n])
+            #     sum_C_32x = torch.sum((label_200_size[n] - pentaly32x_n.cuda()) ** 2)
+
+            #     sum_N_c += sum_C / (C - 1)
+            #     sum_N_c_32x += sum_C_32x / (C - 1)
+            # loss_size = weight[5] * (sum_N_c + sum_N_c_32x) / N_c
+
+            # # Sharhad
+
+            loss = cls_20_loss + (args.subcls_loss_weight * cls_200_loss) + loss_size
+
+            if iter % 100 == 0:
+                print('k{} R{}| Ep:{} L:{} -20_LOSS:{} -200_LOSS:{} SIZE_LOSS:{} | -cls20:{} | -cls200_Top1:{} -Sum:{}'.format(args.k_cluster, args.round_nb, ep, round(loss.item(), 3), round(cls_20_loss.item(), 3), round(cls_200_loss.item(), 3), round(loss_size.item(), 3), avg_cls20_ep_acc, avg_ep_p_acc, avg_ep_acc))
 
             avg_meter.add({'loss': cls_200_loss.item()})
 
@@ -279,6 +346,7 @@ if __name__ == '__main__':
 
             writer.add_scalar('20 Classification Loss', cls_20_loss.item(), step)
             writer.add_scalar('200 Classification Loss', cls_200_loss.item(), step)
+            writer.add_scalar('Size Loss', loss_size.item(), step)
             writer.add_scalar('Total Loss', loss.item(), step)
             writer.add_scalar('Cls20 Accuracy', avg_cls20_ep_acc, step)
             writer.add_scalar('Cls200 Accuracy (Sum)', avg_ep_acc, step)
@@ -286,7 +354,7 @@ if __name__ == '__main__':
 
             step += 1
 
-            if (optimizer.global_step-1)%100 == 0:
+            if (optimizer.global_step - 1) % 100 == 0:
                 timer.update_progress(optimizer.global_step / max_step)
 
                 print('Iter:%5d/%5d' % (optimizer.global_step - 1, max_step),
